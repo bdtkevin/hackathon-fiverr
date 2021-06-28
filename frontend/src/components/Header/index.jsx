f1f53d423a352cd3db030d25c69f2d94763abac9
AntoineCau <acauwd@gmail.com>
2021-06-28 17:03:09 +0200

    style

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,18 +3,18 @@ import './style.scss';
 
 export default function Header() {
   return (
-    <header>
+    <header className='header'>
       <img src='/img/fiverrlogo.png' alt='logo' className='logo' />
       <div className='container'>
         <ul className='team'>
           <li>
             <NavLink to='/create-a-team' className='createTeam'>
-              Create a team
+              create a team
             </NavLink>
           </li>
           <li>
             <NavLink to='/join-a-team' className='joinTeam'>
-              Join a team
+              join a team
             </NavLink>
           </li>
         </ul>
