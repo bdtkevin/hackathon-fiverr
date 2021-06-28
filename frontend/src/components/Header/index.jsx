e5b5c1e569294fdaa9d10f58779c3a5f0e5fdd4a
AntoineCau <acauwd@gmail.com>
2021-06-28 15:59:14 +0200

    adding style on header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,18 +1,25 @@
+import { NavLink } from 'react-router-dom';
 import './style.scss';
 
 export default function Header() {
   return (
     <div className='header'>
       <img src='/img/fiverrlogo.png' alt='logo' className='logo' />
-      <ul className='team'>
-        <li>
-          <div className='createTeam'>Create a team</div>
-        </li>
-        <li>
-          <div className='joinTeam'>Join a team</div>
-        </li>
-      </ul>
-      <img src='/img/user-logo.png' alt='userlogo' className='userLogo' />
+      <div className='container'>
+        <ul className='team'>
+          <li>
+            <NavLink to='/create-a-team' className='createTeam'>
+              Create a team
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/join-a-team' className='joinTeam'>
+              Join a team
+            </NavLink>
+          </li>
+        </ul>
+        <img src='/img/user-logo.png' alt='userlogo' className='userLogo' />
+      </div>
     </div>
   );
 }
