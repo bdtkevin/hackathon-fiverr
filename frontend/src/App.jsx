d60a2454e812d756537a4119cf59ed0590657664
thomassupervil <thomas.supervil@gmail.com>
2021-06-28 23:08:20 +0200

    create footer and start create team component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
+import CreateTeam from './components/CreateTeam';
+
 import Toto from './components/Toto';
 import './reset.css';
 
 export default function App() {
   return (
     <div className='App'>
-      <Header />
+
       <Switch>
         <Route path='/toto' component={Toto} />
+        <Route path='/create-team' component={CreateTeam} />
       </Switch>
     </div>
   );
---
+++ frontend/src/components/CreateTeam/index.jsx
@@ -0,0 +1,56 @@
+import {useState} from 'react'
+import Header from '../Header';
+import Footer from '../Footer'
+import './style.scss'
+
+const CreateTeam = ()=>{
+
+const [category,setCategory] = useState('');
+const [job, setJob] = useState('');
+
+  return (<>
+    <Header />
+    <section className="banner">
+      <div className="textContainer">
+        <h1>
+          Create my team
+        </h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit involuptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <img src="/img/createTeamImg/teamImg.svg" alt="" />
+    </section>
+    <section className="domain">
+      <button onClick={()=>setCategory("web")} className={category==="web"?"webContainer selected":"webContainer"} >
+        <h2 className="title">
+          Web
+        </h2>
+        <img src="/img/createTeamImg/webIllu.svg" alt="" />
+      </button>
+      <button className={category==="graphism"?"graphismContainer selected":"graphismContainer"} onClick={()=>setCategory("graphism")}>
+        <h2>
+          Graphism
+        </h2>
+        <img src="/img/createTeamImg/graphismIllu.svg" alt="" />
+      </button>
+      <button className={category==="video"?"videoContainer selected":"videoContainer"} onClick={()=>setCategory("video")}>
+        <h2>
+          Video making
+        </h2>
+        <img src="/img/createTeamImg/videoIllu.svg" alt="" />
+      </button>
+    </section>
+    <section className="criteria">
+      <p>I'm a 
+        <select onChange={(e)=>setJob(e.target.value)} value={job}>
+        {category === "web" && <><option value="developper web">Developper web</option><option value="UI designer">UI designer</option><option value="UX designer">UX designer</option></>}
+        {category === "graphism" && <><option value="director artistic">Director artistic</option><option value="graphist">Graphist</option><option value="redactor">Redactor</option></>}
+        {category === "video" && <><option value="editor">Editor</option><option value="animator">Animator</option><option value="sound designer">Sound designer</option></>}
+        </select>looking for friendly people to collaborate !</p>
+    </section>
+    <Footer />
+</>
+  )
+}
+export default CreateTeam;
