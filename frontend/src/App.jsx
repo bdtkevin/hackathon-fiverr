66fd34699cc0e1cb2902b7674aee4a06d086987e
AntoineCau <acauwd@gmail.com>
2021-06-29 10:13:13 +0200

    deleting toto component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
 import CreateTeam from './components/CreateTeam';
 import WebClient from './components/WebClient';
-import Toto from './components/Toto';
 import './reset.css';
 
 export default function App() {
   return (
     <div className='App'>
       <Switch>
-        <Route path='/toto' component={Toto} />
         <Route path='/create-team' component={CreateTeam} />
         <Route path='/webclient' component={WebClient} />
       </Switch>
